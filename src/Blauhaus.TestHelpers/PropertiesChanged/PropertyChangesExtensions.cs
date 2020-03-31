12dcaebc82b15065631337a136fbc83274c22099
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-31 16:49:09 +0200

    no message

---
+++ src/Blauhaus.TestHelpers/PropertiesChanged/PropertyChangesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using Blauhaus.TestHelpers.PropertiesChanged.NotifyPropertyChanged;
+
+namespace Blauhaus.TestHelpers.PropertiesChanged
+{
+    public static class PropertyChangesExtensions
+    {
+        public static bool IsSequenceEqual<TNotifyPropertyChanged, T>(this PropertyChanges<TNotifyPropertyChanged, T> propertyChanges, params T[] expectedSequence) where TNotifyPropertyChanged : INotifyPropertyChanged
+        {
+            return expectedSequence.SequenceEqual(propertyChanges);
+        }
+
+    }
+}
