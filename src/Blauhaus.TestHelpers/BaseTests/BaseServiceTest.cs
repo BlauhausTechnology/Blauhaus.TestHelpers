b86da5e3873fa28d34603da5185370c77a687acc
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-03 17:33:33 +0200

    BaseServiceTest

---
+++ src/Blauhaus.TestHelpers/BaseTests/BaseServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.TestHelpers.BaseTests
+{
+    public class BaseServiceTest<TSut> : BaseUnitTest<TSut> where TSut : class
+    {
+        protected IServiceCollection Services;
+
+        protected override TSut ConstructSut()
+        {
+            return Services.BuildServiceProvider().GetRequiredService<TSut>();
+        }
+
+
+        protected override void Cleanup()
+        {
+            base.Cleanup();
+            Services = new ServiceCollection();
+        }
+    }
+}
