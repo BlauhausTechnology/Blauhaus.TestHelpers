e2f33cbd2e6d1d29dcc0c92691b0b40e2d41d345
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-06 09:31:44 +0200

    no message

--- src/Blauhaus.TestHelpers/Builders/_Base/BaseBuilder.cs
+++ src/Blauhaus.TestHelpers/Builders/_Base/BaseBuilder.cs
@@ -10,8 +10,12 @@ namespace Blauhaus.TestHelpers.Builders._Base
         where TBuilder :  BaseBuilder<TBuilder, T>
     {
 
-        protected readonly IFixture MyFixture;
         private readonly ICustomizationComposer<T> _fixture;
+        private T _object;
+        private Random _random;
+        
+        protected Random Random => _random ??= new Random(); 
+        protected readonly IFixture MyFixture;
 
         protected BaseBuilder()
         {
@@ -21,12 +25,19 @@ namespace Blauhaus.TestHelpers.Builders._Base
 
         public TBuilder With<TProperty>(Expression<Func<T, TProperty>> expression, TProperty value)
         {
-            _fixture.With<TProperty>(expression);
+            _fixture.With(expression);
             return this as TBuilder;
         }
 
-        public virtual T Object => _fixture.Create();
-        
+        public virtual T Object
+        {
+            get
+            {
+                if (_object == null) _object = _fixture.Create();
+                return _object;
+            }
+        }
+
         public static T Default => Activator.CreateInstance<TBuilder>().Object;
         
     }
