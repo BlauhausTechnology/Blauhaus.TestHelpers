22557b23650f1fb8da818c7f2b5597148b79a6be
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-30 12:39:30 +0200

    no message

---
+++ src/Blauhaus.TestHelpers/Extensions/MockContainerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Blauhaus.Common.Utils.Contracts;
+using Blauhaus.TestHelpers.MockBuilders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.TestHelpers.Extensions
+{
+    public static class MockContainerExtensions
+    {
+        public static AsyncPublisherMockBuilder<TMock, T> AddMockAsyncPublisher<TMock, T>(this MockContainer mocks) where TMock : class, IAsyncPublisher<T>
+        {
+            var mock = new AsyncPublisherMockBuilder<TMock, T>();
+            mocks.AddMock<AsyncPublisherMockBuilder<TMock, T>, TMock>();
+            return mock;
+        }
+    }
+}
---
+++ src/Blauhaus.TestHelpers/MockBuilders/AsyncPublisherMockBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blauhaus.Common.Utils.Contracts;
+using Moq;
+
+namespace Blauhaus.TestHelpers.MockBuilders
+{
+
+    public abstract class BaseAsyncPublisherMockBuilder<TBuilder, TMock, T> : BaseMockBuilder<TBuilder, TMock>
+        where TBuilder : BaseAsyncPublisherMockBuilder<TBuilder, TMock, T>
+        where TMock : class, IAsyncPublisher<T>
+    {
+        private readonly List<Func<T, Task>> _handlers = new List<Func<T, Task>>();
+
+        public Mock<IDisposable> AllowMockSubscriptions()
+        {
+            var mockToken = new Mock<IDisposable>();
+
+            Mock.Setup(x => x.SubscribeAsync(It.IsAny<Func<T, Task>>()))
+                .Callback((Func<T, Task> handler) =>
+                {
+                    _handlers.Add(handler);
+                }).ReturnsAsync(mockToken.Object);
+
+            return mockToken;
+        }
+        
+        public async Task PublishMockSubscriptionAsync(T model)
+        {
+            foreach (var handler in _handlers)
+            {
+                await handler.Invoke(model);
+            }
+        }
+    }
+
+    public class AsyncPublisherMockBuilder<TMock, T> : BaseAsyncPublisherMockBuilder<AsyncPublisherMockBuilder<TMock, T>, TMock, T>
+        where TMock : class, IAsyncPublisher<T>
+    {
+        
+      
+    }
+}
