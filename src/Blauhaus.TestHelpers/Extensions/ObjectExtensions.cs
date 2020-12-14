1df501ad50efc6078386ad2daa51dce8174ad0f5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-14 08:31:42 +0200

    no message

---
+++ src/Blauhaus.TestHelpers/Extensions/ObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Blauhaus.TestHelpers.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Returns when the preficate is true or the timeout expires
+        /// </summary>
+        public static T WaitFor<T>(this T obj,  Expression<Func<T, bool>> predicate, int timeoutMs = 1000)
+        {
+            var startMs = DateTime.UtcNow.Ticks/10000;
+            while (!predicate.Compile().Invoke(obj))
+            {
+                if (DateTime.UtcNow.Ticks/10000 - startMs >= timeoutMs)
+                {
+                    break;
+                }
+            }
+            return obj;
+        }
+    }
+}
