ab8f9f12d7f52979910ff2848a79f4836cb6f08b
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-27 10:39:19 +0200

    no message

--- src/Blauhaus.TestHelpers/Builders/_Base/IBuilder.cs
+++ src/Blauhaus.TestHelpers/Builders/_Base/IBuilder.cs
@@ -3,10 +3,15 @@ using System.Linq.Expressions;
 
 namespace Blauhaus.TestHelpers.Builders._Base
 {
-    public interface IBuilder<TBuilder, T>
+
+    public interface IBuilder<out T>
+    {
+        T Object { get; }
+    }
+    
+    public interface IBuilder<out TBuilder, T> : IBuilder<T>
     {
         TBuilder With<TProperty>(Expression<Func<T, TProperty>> expression, TProperty value);
-        T Object { get; }
 
     }
 }
