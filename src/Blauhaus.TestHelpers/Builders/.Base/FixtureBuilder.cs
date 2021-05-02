8fca67975e72c083d82a6415375952cb0dfa806c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-05-02 13:15:42 +0200

    no message

--- src/Blauhaus.TestHelpers/Builders/.Base/FixtureBuilder.cs
+++ src/Blauhaus.TestHelpers/Builders/.Base/FixtureBuilder.cs
@@ -1,6 +1,6 @@
 ﻿namespace Blauhaus.TestHelpers.Builders.Base
 {
-    public class FixtureBuilder<T> : BaseReadonlyFixtureBuilder<FixtureBuilder<T>, T> where T : class
+    public class FixtureBuilder<T> : BaseRecordFixtureBuilder<FixtureBuilder<T>, T> where T : class
     {
         
     }
