b54cac623c964326a4575591e993913dbd20b1cd
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-07-03 10:35:27 +0200

    no message

---
+++ src/Blauhaus.TestHelpers/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using Microsoft.Extensions.Options;
+
+namespace Blauhaus.TestHelpers.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static TOptions AddMockOptions<TOptions>(this IServiceCollection services, Func<TOptions> optionsBuilder) where TOptions : class, new()
+        {
+            var options = new TOptions();
+            var mockOptions = new Mock<IOptions<TOptions>>();
+            mockOptions.Setup(x => x.Value).Returns(optionsBuilder);
+            services.AddSingleton(mockOptions.Object);
+
+            return options;
+        }
+    }
+}
